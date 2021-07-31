4ec7a07b65aae921a23facbb07fa57def3c608fe
sunanda-567 <77392175+sunanda-567@users.noreply.github.com>
2021-07-31 13:52:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas=createCanvas(300,300);
+    canvas.center();
+}
+function draw() {
+
+}
+function takesnapshot() {
+    save('my_pic.png')
+}
